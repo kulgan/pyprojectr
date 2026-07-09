921522aa92dc94b2686ddaf044547a4edb79018d
Rowland Ogwara <r.ogwara@neem.works>
2026-07-08 23:34:58 -0500

    add version variable

--- src/pyprojectr/__init__.py
+++ src/pyprojectr/__init__.py
@@ -1,8 +1,13 @@
+from importlib.metadata import distribution
+
 from pyprojectr import pyproject
 from pyprojectr.core import BaseModel, PyProjectTool
 from pyprojectr.pyproject import Author, BuildSystem, License, Maintainer, PyProject, PyProjectFile, Readme
 
+VERSION = distribution(__name__).version
+
 __all__ = [
+    "VERSION",
     "Author",
     "BaseModel",
     "BuildSystem",
